eda97a95688a3292cc83a1c0589eb4247d78ade2
Isaac <isaachill13@gmail.com>
2019-03-20 20:36:26 +1000

    hero image working

--- src/components/Wrapper.jsx
+++ src/components/Wrapper.jsx
@@ -4,6 +4,7 @@ const Wrapper = styled.div`
   max-width: ${props => props.theme.maxWidth};
   margin: 0 auto;
   padding: 0 2rem;
+  margin-bottom: -15rem;
   @media (max-width: ${props => props.theme.breakpoints.m}) {
     padding: 0 1.5rem;
   }
